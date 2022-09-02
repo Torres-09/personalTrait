e3d07da64a75da59ba333cd4a9d5933423a702fb
imsehwan <dla612@naver.com>
2022-09-03 03:12:22 +0900

    Initial commit

---
+++ src/main/java/Hwan2da/personalTrait/PersonalTraitApplication.java
@@ -0,0 +1,13 @@
+package Hwan2da.personalTrait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTraitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTraitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hwan2da/personalTrait/PersonalTraitApplicationTests.java
@@ -0,0 +1,13 @@
+package Hwan2da.personalTrait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTraitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
